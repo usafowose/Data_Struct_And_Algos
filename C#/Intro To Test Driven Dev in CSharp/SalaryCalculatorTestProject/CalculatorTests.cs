87f48ba2bb6ca1612f55a10ea4097a8cfcc4a5fe
Andrew Fowose (Aerotek US) <v-afowose@microsoft.com>
2021-03-09 10:43:28 -0500

    Added red failing tests to implement red, green, refactor

--- C#/Intro To Test Driven Dev in CSharp/SalaryCalculatorTestProject/CalculatorTests.cs
+++ C#/Intro To Test Driven Dev in CSharp/SalaryCalculatorTestProject/CalculatorTests.cs
@@ -2,12 +2,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SalaryCalculatorTestProject
 {
+
+    /* 
+     * To get hourly, divide annual salary by 2080
+     * 100,006.40 / 2080 = $48.08/hr
+     * 
+     * To get annual, multiply hourly by 2080
+     * $48.08 * 2080 = $100,006.49
+     */
+
     [TestClass]
-    public class UnitTest1
+    public class CalculatorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AnnualSalaryTest()
         {
+            // Arrange 
+            SalaryCalculatorTestProject sc = new SalaryCalculator();
+
+            // Act
+            decimal annualSalary = sc.GetAnnualSalary(50);
+
+            // Assert
+            Assert.AreEqual(104000, annualSalary); 
         }
     }
 }
