05b419da6581918568288efb47c641f625c05ab1
Andrew Fowose (Aerotek US) <v-afowose@microsoft.com>
2021-03-10 01:03:10 -0500

    Adding test project to test DescendingOrder file

---
+++ C#/Kata_Coding_Challenges/DescendingOrderTestProj/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DescendingOrderTestProj
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
