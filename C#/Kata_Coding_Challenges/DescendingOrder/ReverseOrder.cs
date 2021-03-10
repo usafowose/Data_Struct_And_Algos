5fedf12be0b079c1b30adc9db2658e86df430076
Andrew Fowose (Aerotek US) <v-afowose@microsoft.com>
2021-03-10 01:29:22 -0500

    Added test method in test file and ReverseOrder as prject dependncy of test file

--- C#/Kata_Coding_Challenges/DescendingOrder/ReverseOrder.cs
+++ C#/Kata_Coding_Challenges/DescendingOrder/ReverseOrder.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 
 namespace DescendingOrder
 {
-    class Program
+    public class ReverseOrder
     {
 
         // Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 
         // Examples:
         // Input: 42145 Output: 54421
-        public static int Descending(int number)
+        public int Descending(int number)
         {
             List<int> NumList = new List<int>();
             string numStr = number.ToString();
@@ -36,10 +36,6 @@ namespace DescendingOrder
 
            
 
-        }
-        static void Main(string[] args)
-        {
-            Console.WriteLine(Descending(68962153)); 
         }
     }
 }
