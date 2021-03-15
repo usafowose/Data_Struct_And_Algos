73ecdd31ff9679ae1173a1ef5940d43769cf62b3
Andrew Fowose (Aerotek US) <v-afowose@microsoft.com>
2021-03-15 01:11:07 -0400

    Added class library with interfaces and classes to use in console app

---
+++ C#/OOP/Object-Oriented-Programming/All-My-Classes/BasicCoffeeMaker.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace All_My_Classes
+{
+    public class BasicCoffeeMaker : CoffeeMaker
+    {
+        private string name;
+        private int size;
+        private string[] ingredients = new string[2];
+
+        public BasicCoffeeMaker(string name, string ingredient1, string ingredient2, int sizeInOzs) 
+        {
+            this.name = name;
+            size = sizeInOzs;
+            ingredients[0] = ingredient1;
+            ingredients[1] =  ingredient2; 
+        }
+        public string Brew() 
+        {
+            return "Brewing Ingredients"; 
+        }
+
+        public string PourDrink() 
+        {
+            return $"{size}oz. drink Completed In 2 Minutes";
+        }
+    }
+
+
+
+    public class AdvancedCoffeeMaker: CoffeeMaker 
+    {
+        private string name; 
+        private int size;
+        private string[] ingredients = new string[3]; 
+
+        public AdvancedCoffeeMaker(string name, string ingredient1, string ingredient2, string ingredient3, int sizeInOzs)
+        {
+            this.size = sizeInOzs;
+            this.name = name;
+            ingredients[0] = ingredient1;
+            ingredients[1] = ingredient2;
+            ingredients[2] = ingredient3; 
+        }
+
+        public string Brew() 
+        {
+            return "Brewing"; 
+        }
+
+        public string Brew(string brewSpeed) 
+        {
+            if (brewSpeed == "fast") return "Brewing Quickly";
+
+            return $"Brewing It {brewSpeed}ly"; 
+        }
+
+        public string Brew(string brewSpeed, int quantity) 
+        {
+            this.size = quantity; 
+            return $"{brewSpeed}ly brewing your {size}oz. cup of carefully crafted coffee"; 
+        }
+
+
+
+        public string PourDrink() 
+        {
+            return "Pouring Your Daringly Designed Cup!";
+        }
+
+        public string Update() 
+        {
+            return $"Your {this.name} machine is churning out your drink as soon as possible. Slow and steady wins the race.";
+        }
+    }
+
+}
---
+++ C#/OOP/Object-Oriented-Programming/All-My-Classes/CoffeeMaker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All_My_Classes
+{
+    interface CoffeeMaker
+    {
+        public string Brew();
+        public string PourDrink();
+    }
+}
